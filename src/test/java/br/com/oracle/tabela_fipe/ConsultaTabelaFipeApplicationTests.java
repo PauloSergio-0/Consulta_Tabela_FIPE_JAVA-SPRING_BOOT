fd2c5f53e4dd3aa84d64caa74507446e0331adc8
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-05-25 16:37:12 -0300

    feat: init project

---
+++ src/test/java/br/com/oracle/tabela_fipe/ConsultaTabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oracle.tabela_fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaTabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
