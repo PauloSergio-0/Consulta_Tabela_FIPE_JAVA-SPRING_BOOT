a699f1065fcd26bbfae8f43e7ad9f71296c612aa
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-05-29 23:00:49 -0300

    feat: added module of connection with FIPE API

---
+++ src/main/java/br/com/oracle/tabela_fipe/service/ConsumoApi.java
@@ -0,0 +1,28 @@
+package br.com.oracle.tabela_fipe.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoApi {
+
+    public String obterDados(String endereco) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(endereco))
+                .build();
+
+
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return response.body(); // retorna json
+    }
+}
